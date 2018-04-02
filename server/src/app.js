d7a98659bc023bd549f19c9aada75fdd33609289
unknown <jonathanbarry1@gmail.com>
2018-04-02 21:57:13 +0100

    Setting up the registering page

--- server/src/app.js
+++ server/src/app.js
@@ -8,10 +8,10 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/status', (req, res) =>{
-res.send({
-    message: 'hello world!'
-    });
+app.post('/register', (req, res) =>{
+    res.send({
+        message: 'Hello ' +req.body.email+ 'Your user was registered have fun !!!'
+        });
 });
 
 app.listen(process.env.PORT || 8081);
